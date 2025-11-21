a6f59a7be05ec01561cdca2691a10584a220bc1f
MT <burnerforthee@proton.me>
2025-11-22 02:33:42 +0700

    Set secure cookie for theme

--- app/sessions.server.tsx
+++ app/sessions.server.tsx
@@ -2,7 +2,7 @@ import { createCookieSessionStorage } from 'react-router'
 import { createThemeSessionResolver } from 'remix-themes'
 
 // You can default to 'development' if process.env.NODE_ENV is not set
-// const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production'
 
 const sessionStorage = createCookieSessionStorage({
   cookie: {
@@ -12,9 +12,9 @@ const sessionStorage = createCookieSessionStorage({
     sameSite: 'lax',
     secrets: ['s3cr3t'],
     // Set domain and secure only if in production
-    // ...(isProduction
-    //   ? { domain: 'your-production-domain.com', secure: true }
-    //   : {}),
+    ...(isProduction
+      ? { domain: 'https://aha-vault.vercel.app/', secure: true }
+      : {}),
   },
 })
 
