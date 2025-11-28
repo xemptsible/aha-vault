7c25b08fdeb2f5a2d753a79e928873da63866c1c
MT <burnerforthee@proton.me>
2025-11-29 02:35:05 +0700

    Update data type

--- app/types/author.ts
+++ app/types/author.ts
@@ -1,7 +1,10 @@
+import type { Resource } from './resource'
+
 export interface Author {
   id: number
   name: string
   personal_site: string
+  credited_resources: Array<Resource>
 }
 
 export interface Authors {
--- app/types/tag.ts
+++ app/types/tag.ts
@@ -1,6 +1,9 @@
+import type { Resource } from "./resource"
+
 export interface Tag {
   id: number
   name: string
+  related_resources: Array<Resource>
 }
 
 export interface Tags {
