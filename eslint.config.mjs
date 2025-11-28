8476ef649379c11c321640041d8213da18d5fbe1
MT <burnerforthee@proton.me>
2025-11-29 01:56:38 +0700

    Add Eslint for a11y

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,13 @@ import eslint from '@eslint/js'
 import reactHooks from 'eslint-plugin-react-hooks'
 import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 
 export default defineConfig(
   {
     ignores: ['node_modules/**', '.react-router/**', 'build/**', 'out/**'],
   },
+  jsxA11y.flatConfigs.recommended,
   reactHooks.configs.flat.recommended,
   eslint.configs.recommended,
   tseslint.configs.recommended,
