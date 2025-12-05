0fe917f8d829302c77075f9c85ab65ed67ef1c8a
MT <burnerforthee@proton.me>
2025-12-06 03:16:08 +0700

    Update React

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'aha-root.css',
+      },
+    },
+  },
 })
